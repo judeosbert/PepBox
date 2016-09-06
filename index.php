10c4171d8b144afef840575d5238008ca2b965e4
judeosbert <jude.osbertk2014@vit.ac.in>
2016-09-06 11:38:09 +0530

    First Commit with all the framework files and login html

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jude
+ * Date: 6/9/16
+ * Time: 10:52 AM
+ */
+
+?>
+<!Doctype html>
+    <html>
+<head>
+    <title>PepBox Login</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/extraStyleSheet.css" rel="stylesheet" />
+    <link href="assets/css/loginPage.css" rel="stylesheet" />
+    <script src="assets/js/bootstrap.min.js" ></script>
+</head>
+<body>
+<div class="container full-length">
+    <div class="row">
+        <div class="col-lg-6 text-lg-center  ">
+            <div class="logo-signup top40">
+            <p> <img class="logo-img" src="assets/images/company_logo.png" /> </p>
+                <p><button class="btn btn-primary" title="Signup to PepBox">New to GitHub?</button> </p>
+
+            </div>
+
+        </div>
+        <div class="col-lg-6">
+            <div class="login-form top30 text-lg-center">
+                <h1>Welcome!</h1>
+                <div class="form-elements top7">
+                <form id="login-form">
+                    <input type="email" class="form-control rounded-input" placeholder="Email ID" />
+                    <input type="password" class=" top5 form-control rounded-input" placeholder="Password" />
+                    <p><button class=" top7 btn btn-primary" title="Signup to PepBox">Log in</button> </p>
+                    <a href="#">Forgot Password</a>
+                </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="footer">
+            asdas
+        </div>
+    </div>
+</div>
+</body>
+</html>
