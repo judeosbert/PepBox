06034a9c72ea904fa9e1cd219a7706c65e71316c
judeosbert <jude.osbertk2014@vit.ac.in>
2016-09-06 12:42:30 +0530

    First Commit with all the framework files and login html

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <html>
 <head>
     <title>PepBox Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/css/extraStyleSheet.css" rel="stylesheet" />
     <link href="assets/css/loginPage.css" rel="stylesheet" />
@@ -19,14 +20,21 @@
 <body>
 <div class="container ">
     <div class="row">
-        <div class="col-lg-6 text-lg-center">
-            <div class="logo-signup top40">
-            <p> <img class="logo-img" src="assets/images/company_logo.png" /> </p>
+        <div class="col-lg-12">
+            <img class="logo-img" src="assets/images/company_logo.png" />
+        </div>
+    </div>
+    <div class="row">
 
+        <div class="col-lg-8 text-lg-center">
+            <div class="signup-text top14">
+            <h1 class="text-lg-left promote-head">How People Build Software</h1>
+                <p class="text-lg-left promote-text" >Millions of developers use GitHub to build personal projects, support their businesses, and work together on open source technologies</p>
+                <button class="btn btn-primary" title="Signup to PepBox">New to GitHub?</button>
             </div>
 
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-4">
             <div class="login-form top30 text-lg-center">
                 <h1>Welcome!</h1>
                 <div class="form-elements top7">
@@ -36,7 +44,7 @@
                     <p>
                         <ul class="list-inline">
                         <li class="list-inline-item"><button class=" top7 btn btn-primary" title="Signup to PepBox">Log in</button></li>
-                        <li class="list-inline-item"><button class="btn btn-primary" title="Signup to PepBox">New to GitHub?</button></li>
+
                     </ul>
                     </p>
 
